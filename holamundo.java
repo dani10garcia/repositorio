2c5abfdd3fda3ee92503ac30ca54eff9da06c8b2
dani10garcia <dani10garcia1017@gmail.com>
2023-12-21 18:32:25 +0100

    buenasss

---
+++ holamundo.java
@@ -0,0 +1,58 @@
+public class holamundo {
+    public static void main(String[] args){
+
+
+        // for(int contador = 1; contador<= 10; contador++){
+        //     if (contador==7)
+        //     break;
+        //     System.out.println("Valor:" + contador);
+        // }
+
+
+        // int entero = 0;
+        // switch (entero) {
+        //     case 0:
+        //         System.out.println("Hola soy un cero");
+        //         break;
+        //     case 1:
+        //         System.out.println("Hola soy un uno");
+        //         break;
+        //     case 2:
+        //         System.out.println("Hola soy un dos");
+        //         break;    
+        //     default:
+        //         break;
+        // }
+        
+        // String[] jugador = {"delantero", "medio", "defensa","portero" , "entrenador"};
+
+        //     for(int i = 0; i < jugador.length; i++ ){
+        //         switch (jugador [i]) {
+        //             case "delantero":
+        //                 System.out.println("El delantero chuta");
+        //                 break;
+        //             case "medio":
+        //                 System.out.println("El medio pasa el balón");
+        //                 break;
+        //             case "defensa":
+        //                 System.out.println("El defensa para el balón");
+        //                 break;
+        //             case "portero":
+        //                 System.out.println("El portero para el penalti");
+        //                 break;        
+        //             default:
+        //                 System.out.println("El entrenador dirige");
+        //                 break;
+        //         }
+        //     }
+            // holaa
+        for(int contador = 1; contador<= 10; contador++){
+                
+            if (contador % 2 != 0){
+                System.out.println("Valor:" + contador);
+                continue;
+            }
+        }
+
+    }
+}
